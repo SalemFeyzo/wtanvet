65cb36d3cb7556d08b4cae6b03ca8833ca830c6a
Salem Feyzo <f1989salim@gmail.com>
2020-08-21 20:24:30 +0300

    Updates

---
+++ precache-manifest.e89619282c202e020999d29569de22be.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cfde1d99836b940bd0f9727c7af1a0",
+    "url": "/wtanvet/index.html"
+  },
+  {
+    "revision": "edee9b0ed4b7f4352b57",
+    "url": "/wtanvet/static/css/2.f53be0c5.chunk.css"
+  },
+  {
+    "revision": "22bc4dd68b24286e1361",
+    "url": "/wtanvet/static/css/main.54ba2258.chunk.css"
+  },
+  {
+    "revision": "edee9b0ed4b7f4352b57",
+    "url": "/wtanvet/static/js/2.6e4a600b.chunk.js"
+  },
+  {
+    "revision": "9ab377ffadd866c0fc0349e65d8be966",
+    "url": "/wtanvet/static/js/2.6e4a600b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22bc4dd68b24286e1361",
+    "url": "/wtanvet/static/js/main.4a5992fd.chunk.js"
+  },
+  {
+    "revision": "ccf1312759c7b16f8ce1",
+    "url": "/wtanvet/static/js/runtime-main.ff8e7346.js"
+  },
+  {
+    "revision": "6d1e27416bbe7a2bef0708fa4b83b9db",
+    "url": "/wtanvet/static/media/about.6d1e2741.jpg"
+  },
+  {
+    "revision": "9bb8531d888fe909ba7f76e941d765f5",
+    "url": "/wtanvet/static/media/facebook.9bb8531d.svg"
+  },
+  {
+    "revision": "ed8ac6813a3704efafcd4a8aed2a9f48",
+    "url": "/wtanvet/static/media/instagram.ed8ac681.svg"
+  },
+  {
+    "revision": "fec1eec22af02564f76cc95f01eac3a4",
+    "url": "/wtanvet/static/media/jumbtron-img.fec1eec2.jpg"
+  },
+  {
+    "revision": "35733cef32e641a9844edcb550d1ee73",
+    "url": "/wtanvet/static/media/logo.35733cef.jpg"
+  },
+  {
+    "revision": "d5ff1945241b0976e3cf03a84d4444dd",
+    "url": "/wtanvet/static/media/service1.d5ff1945.svg"
+  },
+  {
+    "revision": "50bfe062e18abc000250cc512bfa66c8",
+    "url": "/wtanvet/static/media/sulfamycin.50bfe062.jpg"
+  },
+  {
+    "revision": "e976aa1d7dfaa65879bc122186a11538",
+    "url": "/wtanvet/static/media/twitter.e976aa1d.svg"
+  },
+  {
+    "revision": "40f433564982d102dbb9b473ef539ee2",
+    "url": "/wtanvet/static/media/unisolcfort.40f43356.jpg"
+  }
+]);
