015688e76b4d27ee44205f3d874b25870a48bfe4
Salem Feyzo <f1989salim@gmail.com>
2020-12-03 20:25:41 +0300

    Some editing

--- src/components/layout/agents/Agents.js
+++ src/components/layout/agents/Agents.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Helmet from 'react-helmet';
-import { AgentMap } from './AgentMap';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import useSound from 'use-sound';
-import SoundMp3 from '../../../assets/SoundMp3.mp3';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Helmet from 'react-helmet'
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import useSound from 'use-sound'
+import SoundMp3 from '../../../assets/SoundMp3.mp3'
 
 export const Agents = () => {
-  const [play] = useSound(SoundMp3);
+  const [play] = useSound(SoundMp3)
   return (
     <>
       <Helmet>
-        <meta charSet="utf-8" />
+        <meta charSet='utf-8' />
         <title>وطن فيت | وكلائنا</title>
         <meta
-          name="description"
-          content=" وطن للأدوية البيطرية (وطن فيت), من الشركات الرائدة في مجال الأدوية البيطرية في سوريا. تطبق الشركة تقنيات حديثة بهدف ضمان الفاعالية الممتازة في علاج مختلف الأمراض."
+          name='description'
+          content=' وطن للأدوية البيطرية (وطن فيت), من الشركات الرائدة في مجال الأدوية البيطرية في سوريا. تطبق الشركة تقنيات حديثة بهدف ضمان الفاعالية الممتازة في علاج مختلف الأمراض.'
         />
-        <link rel="canonical" href="https://watanvet.netlify.app/agents" />
+        <link rel='canonical' href='https://watanvet.netlify.app/agents' />
       </Helmet>
       <Breadcrumb>
         <BreadcrumbItem>
-          <Link to="/" onClick={play}>
+          <Link to='/' onClick={play}>
             الرئيسية
           </Link>
         </BreadcrumbItem>
         <BreadcrumbItem active>وكلائنا</BreadcrumbItem>
       </Breadcrumb>
-      <div className="agents-page">
-        <div>
-          <AgentMap />
-        </div>
-        <div>Agents</div>
+      <div className='agents-page'>
+        <div>وكلائنا</div>
       </div>
     </>
-  );
-};
+  )
+}
--- src/components/layout/header/NavItems.js
+++ src/components/layout/header/NavItems.js
@@ -1,54 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from 'reactstrap';
-import useSound from 'use-sound';
-import SoundMp3 from '../../../assets/SoundMp3.mp3';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Nav, NavItem, NavLink } from 'reactstrap'
+import useSound from 'use-sound'
+import SoundMp3 from '../../../assets/SoundMp3.mp3'
 
 export const NavItems = () => {
-  const [play] = useSound(SoundMp3);
+  const [play] = useSound(SoundMp3)
   return (
     <>
-      <Nav className="mr-auto " navbar>
+      <Nav className='mr-auto ' navbar>
         <NavItem>
-          <Link to="/">
+          <Link to='/'>
             <NavLink onClick={play}>الرئيسية</NavLink>
           </Link>
         </NavItem>
-        <UncontrolledDropdown nav inNavbar>
-          <DropdownToggle nav caret onClick={play}>
-            منتجاتنا
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>الشكل الصيدلاني</DropdownItem>
-            <DropdownItem>الحيوانات المعالجة</DropdownItem>
-            <DropdownItem>المجموعة العلاجية</DropdownItem>
-            <DropdownItem divider />
-            <Link to="/products">
-              <DropdownItem onClick={play}>جميع المنتجات</DropdownItem>
-            </Link>
-          </DropdownMenu>
-        </UncontrolledDropdown>
         <NavItem>
-          <Link to="/agents">
+          <Link to='/products'>
+            <NavLink onClick={play}>منتجاتنا</NavLink>
+          </Link>
+        </NavItem>
+        <NavItem>
+          <Link to='/agents'>
             {' '}
             <NavLink onClick={play}>وكلائنا</NavLink>{' '}
           </Link>
         </NavItem>
         <NavItem>
-          <Link to="/contact">
+          <Link to='/contact'>
             {' '}
             <NavLink onClick={play}>اتصل بنا</NavLink>
           </Link>
         </NavItem>
       </Nav>
     </>
-  );
-};
+  )
+}
